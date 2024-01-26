ad2c857330bd46a90ec0c3f5e30a1e3dadb24543
minminchen <ssaa632849@gmail.com>
2024-01-26 13:19:05 +0800

    edit Rotation js

--- Rotation.js
+++ Rotation.js
@@ -4,13 +4,28 @@
 例如:
 當我的陣列為 [2, 4, 6, 8, 10]，並且指定向右平移 3 次，則函式最後將返回 [6, 8, 10, 2, 4]
 因為陣列進行了 3 次旋轉:
-[2, 4, 6, 8, 10] -> [10, 2, 4, 6, 8]
-[10, 2, 4, 6, 8] -> [8, 10, 2, 4, 6]
-[8, 10, 2, 4, 6] -> [6, 8, 10, 2, 4]
+[2, 4, 6, 8, 10] -> [10, 2, 4, 6, 8] 1
+[10, 2, 4, 6, 8] -> [8, 10, 2, 4, 6] 2
+[8, 10, 2, 4, 6] -> [6, 8, 10, 2, 4] 3
 
-
+vscode指令: node Rotation.js
 */
 
 function Rotation(arr, times) {
+    if(arr.length === 0 || times <= 0) {
+        console.log("ANS: ", arr);
+        return arr;
+    }
 
+    let calTimes = arr.length - times;
+
+    // slice two parts before concat
+    let rotatedPart1 = arr.slice(0, calTimes);
+    let rotatedPart2 = arr.slice(-times);
+    let newRotated = rotatedPart2.concat(rotatedPart1);
+    console.log("ANS: ", newRotated);
+
+    return newRotated;
 }
+
+Rotation([2, 4, 6, 8, 10], 4)
