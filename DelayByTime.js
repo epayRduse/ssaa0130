f8a301ae12475ff460c1ffb074e73f814295a082
minminchen <ssaa632849@gmail.com>
2024-01-26 13:20:22 +0800

    edit DelayByTime js

--- DelayByTime.js
+++ DelayByTime.js
@@ -1,7 +1,34 @@
 /**
 請寫一個函式並輸入一個數字，此數字為指定等待的秒數，抵達秒數時才返回結果 "Success"，請搭配使用 Promise 或是 async/await 來撰寫此函數。如果指定的數字小於或等於 0，則函數會返回 "Invalid Time"。
+
+vscode指令: node DelayByTime.js
 */ 
 
-function DelayByTime() {
-  
+// wait time
+function delayByTimeFn(seconds) {
+    return new Promise((resolve, reject) => {
+        if (seconds <= 0) {
+            reject("Invalid Time");
+        }
+        else {
+            setTimeout(() => {
+                resolve("Success")
+            },seconds * 1000);
+        }
+    });
 }
+
+// wait delayByTimeFn method result
+async function DelayByTime() {
+    try {
+        let result = await delayByTimeFn(3);
+        console.log("Result: " ,result);
+        return result;
+    }
+    catch (error) {
+        console.error("Error: ", error);
+        return error;
+    }
+}
+
+DelayByTime();
